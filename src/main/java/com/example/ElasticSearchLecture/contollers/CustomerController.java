a546b25d4e19abbffd0bf15dfc29bc0b4d34cfb4
Murat Ertik <dmuratertik1@gmail.com>
2026-01-27 22:50:33 +0300

    2 endpoint added.

--- src/main/java/com/example/ElasticSearchLecture/contollers/CustomerController.java
+++ src/main/java/com/example/ElasticSearchLecture/contollers/CustomerController.java
@@ -2,20 +2,35 @@ package com.example.ElasticSearchLecture.contollers;
 
 import com.example.ElasticSearchLecture.domain.Customer;
 import com.example.ElasticSearchLecture.repositories.CustomerRepository;
+import com.example.ElasticSearchLecture.requests.CustomerSaveRequest;
+import com.example.ElasticSearchLecture.services.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("customer")
+@RequestMapping("/customer")
 public class CustomerController {
 
-    private final CustomerRepository customerRepository;
+    private final CustomerService customerService;
 
-    @GetMapping("/save")
-    public void save() {
-        customerRepository.save(Customer.builder().name("murat").build());
+//    @GetMapping("/save")
+//    public void save() {
+//        customerRepository.save(Customer.builder().name("murat").build());
+//    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Customer> saveCustomer(@RequestBody CustomerSaveRequest customer) {
+
+        return ResponseEntity.ok(customerService.saveCustomer(customer));
+
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<Iterable<Customer>> getAllCustomers() {
+        return ResponseEntity.ok(customerService.getAllCustomer());
     }
 }
