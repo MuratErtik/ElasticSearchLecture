75785f8bdf5eb7bd016026e20294fdd9118ea876
Murat Ertik <dmuratertik1@gmail.com>
2026-01-27 14:26:36 +0300

    first commit.

---
+++ src/main/java/com/example/ElasticSearchLecture/ElasticSearchLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.ElasticSearchLecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchLectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ElasticSearchLecture/ElasticSearchLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ElasticSearchLecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
