101816dd47eced9be3092556307d87ed882a062f
Murat Ertik <dmuratertik1@gmail.com>
2026-01-27 22:50:58 +0300

    created.

---
+++ src/main/java/com/example/ElasticSearchLecture/requests/CustomerSaveRequest.java
@@ -0,0 +1,25 @@
+package com.example.ElasticSearchLecture.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Range;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerSaveRequest {
+
+    String name;
+
+    String lastName;
+
+    Long age;
+
+
+}
